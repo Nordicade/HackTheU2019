e400d81316f435537079495b607f63e91f362973
Nordicade <46762778+Nordicade@users.noreply.github.com>
2019-10-05 20:36:21 -0600

    First Draft of MongoDB
    Still need to test whether the connection string finds DB cluster

---
+++ MongoDB.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using MongoDB.Driver;
+using System.Threading.Tasks;
+
+public class MongoDB
+{
+    //This is the default constructor
+	public MongoDB()
+	{
+        var connectionString = "mongodb + srv://Test_Admin:<drowssap>@hacktheu-htus5.gcp.mongodb.net/admin?retryWrites=true&w=majority";
+        var client = new MongoClient(connectionString);
+    }
+}
